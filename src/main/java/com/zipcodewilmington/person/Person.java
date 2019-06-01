35eb93562dd2c844a65faf4d000e5d42bea21206
Anish Patel <anishpatel@zipcoders-MacBook-Pro-9.local>
2019-06-01 13:40:26 -0400

    Finished

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -6,30 +6,43 @@ package com.zipcodewilmington.person;
 public class Person {
     private String name;
     private int age;
+    private Integer heightInches = 0;
+
 
     public Person() {
+        name = "";
+        age = Integer.MAX_VALUE;
+
     }
 
     public Person(int age) {
+        this.age = age;
     }
 
     public Person(String name) {
+        this.name = name;
     }
 
     public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public void setAge(int age) {
+        this.age = age;
     }
 
     public String getName() {
-        return null;
+
+        return name;
     }
 
     public Integer getAge() {
-        return null;
+
+        return age;
     }
 }
